638f1f8c4a31f4926832e07c70a4210ceac8784a
Chondan <chondansusuwan@outlook.com>
2021-07-16 23:43:34 +0700

    WIP LEETCODE 16JULY2021

---
+++ 16_JULY_2021/nextGreatElement1.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2);
+};
+
+int main(void) {
+
+}
+
+vector<int> Solution::nextGreaterElement(vector<int> &nums1, vector<int> &nums2) {
+
+    vector<int> ans;
+    
+
+}
