fed9a9efd9ea1d7e0d50641bf8a8ef4c42d4bd8a
Chondan <chondansusuwan@outlook.com>
2021-07-23 22:01:57 +0700

    Update util.h
    * Add class Node for N-ary Tree

--- util/util.h
+++ util/util.h
@@ -24,6 +24,9 @@ int charToInt(char c);
 char intToChar(int n);
 bool isUpperCase(char c);
 
+// Node
+class Node;
+
 // ---- METHODS IMPLEMENTATION ----
 template <typename T>
 void displayVector(vector<T> input) {
@@ -86,5 +89,22 @@ bool isUpperCase(char c) {
     if (ascii < 65 || ascii > 90) return false;
     return true;
 }
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
 
 #endif
