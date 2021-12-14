b2f03d72a3b98f50d74bba86d645358b9f147fd7
Chondan Susuwan <chondansusuwan@outlook.com>
2021-12-15 06:23:55 +0700

    Re-structure

--- languages/go/divisibleSumPairs.go
+++ languages/go/divisibleSumPairs.go
@@ -1,6 +1,13 @@
 package main 
 import ("fmt")
 
+func main() {
+    var numbers = []int32{1,2,3,4,5,6}
+    var k int32 = 5
+    var result int32 = divisibleSumPairs(numbers, k)
+    fmt.Println("result:", result)
+}
+
 func divisibleSumPairs(numbers []int32, k int32) int32 {
     var count int32 = 0
     var numbersLength = len(numbers)
@@ -15,9 +22,3 @@ func divisibleSumPairs(numbers []int32, k int32) int32 {
     return count
 }
 
-func main() {
-    var numbers = []int32{1,2,3,4,5,6}
-    var k int32 = 5
-    var result int32 = divisibleSumPairs(numbers, k)
-    fmt.Println("result:", result)
-}
