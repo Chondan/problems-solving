563e0eb1bdf92a2658936071b31051c2a4ab5c22
Chondan Susuwan <chondansusuwan@outlook.com>
2023-11-07 16:48:58 +0700

    Minimum Depth of Binary Tree
    Leetcode, 07 Nov 2023

---
+++ languages/cpp/minimumDepthOfBinaryTree.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "utils/util.h"
+using namespace std;
+
+class Solution {
+public:
+	int minDepth(TreeNode* root);
+};
+
+int main(void) {
+	Solution solution;
+
+	TreeNode* treeNode1 = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+	int ans1 = solution.minDepth(treeNode1);
+	cout << "ans1: " << ans1 << endl;
+
+	TreeNode* treeNode2 = new TreeNode(2, NULL, new TreeNode(3, NULL, new TreeNode(4, NULL, new TreeNode(5, NULL, new TreeNode(6)))));
+	int ans2 = solution.minDepth(treeNode2);
+	cout << "ans2: " << ans2 << endl;
+}
+
+int Solution::minDepth(TreeNode* root) {
+	if (root == NULL) return 0;
+
+	if (root->left == NULL and root->right != NULL) {
+		return 1 + this->minDepth(root->right);
+	} else if (root->right == NULL and root->left != NULL) {
+		return 1 + this->minDepth(root->left);
+	} else {
+		return 1 + min(this->minDepth(root->left), this->minDepth(root->right));
+	}
+}
