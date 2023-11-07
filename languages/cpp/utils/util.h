ab3b35ad19957e28ddf5173523df6a6ca6f3ff18
Chondan Susuwan <chondansusuwan@outlook.com>
2023-11-07 20:17:29 +0700

    Add struct of LinkedList

--- languages/cpp/utils/util.h
+++ languages/cpp/utils/util.h
@@ -63,9 +63,9 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
 };
 
 void indorder(TreeNode *root) {
@@ -105,6 +105,12 @@ public:
         val = _val;
         children = _children;
     }
+};
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(NULL) {}
 };
 
 #endif
