aa23291e4d54769644460b9196d9ad64c1002150
Chondan Susuwan <chondansusuwan@outlook.com>
2023-11-07 20:15:20 +0700

    Add description to the problem "Minimum Depth Of Binary Tree"

--- languages/cpp/minimumDepthOfBinaryTree.cpp
+++ languages/cpp/minimumDepthOfBinaryTree.cpp
@@ -2,6 +2,14 @@
 #include "utils/util.h"
 using namespace std;
 
+/* 
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node 
+down to the nearest leaf node.
+
+Ref: https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+*/
 class Solution {
 public:
 	int minDepth(TreeNode* root);
